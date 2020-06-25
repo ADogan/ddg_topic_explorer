3ac8d6f316d4f1900f3ff7f19af434e93c99f96d
Ali Dogan <alidogan.just.for.commits@gmail.com>
2020-06-25 16:39:06 +0200

    Add Vis library and a basic network graph

--- ddgtopicsexplorer/src/app/app.component.ts
+++ ddgtopicsexplorer/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 
+import { Network, Edge, Options, Node as NetworkNode} from 'vis-network';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,17 +13,55 @@ export class AppComponent implements OnInit {
 
   profileForm: FormGroup;
   currentSearch: string;
+  network: Network;
 
   ngOnInit() {
     const firstInput = new FormControl();
 
     this.profileForm = new FormGroup({
-      firstInput: firstInput
+      firstInput
     });
+    this.setupNetwork();
 
   }
 
   saveSearch() {
     this.currentSearch = this.profileForm.controls.firstInput.value;
+  }
+
+  setupNetwork() {
+
+  const nodes: NetworkNode[] =  [
+    {id: 1, label: 'Node 1'},
+    {id: 2, label: 'Node 2'},
+    {id: 3, label: 'Node 3'},
+    {id: 4, label: 'Node 4'},
+    {id: 5, label: 'Node 5'},
+  ];
+
+  const edges: Edge[] = [
+    {from: 1, to: 3},
+    {from: 1, to: 2},
+    {from: 2, to: 4},
+    {from: 2, to: 5},
+  ];
+
+  const container = document.getElementById('ddgTopicNetwork');
+  const data = {
+    nodes,
+    edges
+  };
+  const options: Options = {
+    interaction: {
+      dragNodes: true,
+      dragView: true,
+    },
+    physics: {
+      enabled: false
+    }
+
+  };
+
+  this.network = new Network(container, data, options);
   }
 }
