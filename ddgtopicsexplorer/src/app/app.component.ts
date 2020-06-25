46a9c17d6a5539bc110f5132e9d7288805065a85
Ali Dogan <alidogan.just.for.commits@gmail.com>
2020-06-25 13:24:15 +0200

    Add reactive form

--- ddgtopicsexplorer/src/app/app.component.ts
+++ ddgtopicsexplorer/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ddgtopicsexplorer';
+
+  profileForm: FormGroup;
+  currentSearch: string;
+
+  ngOnInit() {
+    const firstInput = new FormControl();
+
+    this.profileForm = new FormGroup({
+      firstInput: firstInput
+    });
+
+  }
+
+  saveSearch() {
+    this.currentSearch = this.profileForm.controls.firstInput.value;
+  }
 }
